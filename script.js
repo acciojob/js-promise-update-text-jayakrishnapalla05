1051add496265e56f5cc34e065b59bd8b2bea921
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-23 13:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function prom(){
+	let a=new Promise((resolve) =>{
+		resolve("Hello, world!");
+	}, 1000);
+	return a;
+}
+let Element=document.getElementById("output");
+prom().then((item) => Element.textContent= item);
